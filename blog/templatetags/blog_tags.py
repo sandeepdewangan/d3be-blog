6658b40f544ed9abcd0cd4719b86275cff3e9a34
sandeepdewangan <sandeepinwork@gmail.com>
2020-09-19 18:20:08 +0530

    added custom filter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
+from django.utils.safestring import mark_safe
 # from project
 from ..models import Post
+# third party
+import markdown
 
 
 register = template.Library()
@@ -18,3 +21,7 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
